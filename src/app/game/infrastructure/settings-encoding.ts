1f18cdd43a06b1ae21af4edc609cc98c7b23ec5a
gimmeh <gimmeh@gimmeh.dev>
2025-07-10 02:33:16 +0200

    feat: Make code appear more random
    Uses a one-time pad technique to make the game code completely random, revealing no information about its state

--- src/app/game/infrastructure/settings-encoding.ts
+++ src/app/game/infrastructure/settings-encoding.ts
@@ -4,6 +4,7 @@ import type { GameCode } from "@/app/game/domain/game"
 import { ALL_SEAT_COUNTS, MIN_SEAT_COUNT, type SeatCount } from "@/app/game/domain/player"
 import { type GameSettings, createGameSettings } from "@/app/game/domain/settings"
 import {
+    type Bit,
     type BitArray,
     bitsRequiredFor,
     bitsToBigInt,
@@ -12,11 +13,11 @@ import {
     extractBits,
     numberToBits,
 } from "@/util/BitArray"
-import { decodeStringToBits, encodeBitsToString } from "@/util/number-encoding"
+import { BITS_PER_CHAR, decodeStringToBits, encodeBitsToString } from "@/util/number-encoding"
 
 const CHECKSUM_BITS = 3 // 3 bits for checksum, giving a false positive rate of 1 in 8
-const DATE_OFFSET_BITS = 1 // 1 bit for date offset (0 or 1), to keep the game alive for at least 1 day
 const ENTROPY_BITS = 6 // 6 bits for entropy, allowing for 64 different values
+const DATE_OFFSET_BITS = 1 // 1 bit for date offset (0 or 1), to keep the game alive for at least 1 day
 const SEAT_COUNT_BITS = bitsRequiredFor(ALL_SEAT_COUNTS.length)
 
 /**
@@ -38,22 +39,23 @@ export function encodeGameSettings(settings: GameSettings, entropy?: number): Ga
     const dateOffset = Math.floor(Date.now() / (1000 * 60 * 60 * 24)) % Math.pow(2, DATE_OFFSET_BITS)
 
     const combined: BitArray = []
-    combined.push(...moduleBits)
+    combined.push(...moduleBits.reverse())
     combined.push(...numberToBits(seatCountOverMin, SEAT_COUNT_BITS))
-    combined.push(...numberToBits(entropy, ENTROPY_BITS))
     combined.push(...numberToBits(dateOffset, DATE_OFFSET_BITS))
+    combined.push(...numberToBits(entropy, ENTROPY_BITS))
     combined.push(...numberToBits(checksum(combined), CHECKSUM_BITS))
 
-    return encodeBitsToString(combined)
+    return bitsToGameCode(combined)
 }
 
 /**
  * Decodes a game code back into settings.
  */
 export function decodeGameSettings(code: GameCode): GameSettings {
-    const decodedBits = decodeStringToBits(code)
+    const decodedBits = gameCodeToBits(code)
 
-    const decodedChecksum = bitsToNumber(extractBits(decodedBits, CHECKSUM_BITS))
+    const decodedChecksumBits = extractBits(decodedBits, CHECKSUM_BITS)
+    const decodedChecksum = bitsToNumber(decodedChecksumBits)
     if (decodedChecksum !== checksum(decodedBits)) throw new Error("Invalid checksum")
 
     extractBits(decodedBits, DATE_OFFSET_BITS + ENTROPY_BITS)
@@ -72,8 +74,8 @@ export function decodeGameSettings(code: GameCode): GameSettings {
  * The entropy is used to produce unique rng values for the same settings.
  */
 export function getEntropyFromGameCode(code: GameCode): number {
-    const decodedBits = decodeStringToBits(code)
-    extractBits(decodedBits, CHECKSUM_BITS + DATE_OFFSET_BITS)
+    const decodedBits = gameCodeToBits(code)
+    extractBits(decodedBits, CHECKSUM_BITS)
     const entropyBits = extractBits(decodedBits, ENTROPY_BITS)
     return bitsToNumber(entropyBits)
 }
@@ -82,8 +84,8 @@ export function getEntropyFromGameCode(code: GameCode): number {
  * Returns the amount of days since the game was created, if today (0) or yesterday (1).
  */
 export function getDateOffsetFromGameCode(code: GameCode): number {
-    const decodedBits = decodeStringToBits(code)
-    extractBits(decodedBits, CHECKSUM_BITS)
+    const decodedBits = gameCodeToBits(code)
+    extractBits(decodedBits, CHECKSUM_BITS + ENTROPY_BITS)
     const gameDateOffsetBits = extractBits(decodedBits, DATE_OFFSET_BITS)
     const gameDateOffset = bitsToNumber(gameDateOffsetBits)
     const todayDateOffset = Math.floor(Date.now() / (1000 * 60 * 60 * 24)) % Math.pow(2, DATE_OFFSET_BITS)
@@ -91,6 +93,45 @@ export function getDateOffsetFromGameCode(code: GameCode): number {
 
     if (diff < 0) return diff + Math.pow(2, DATE_OFFSET_BITS) // Adjust for negative difference
     return diff
+}
+
+function bitsToGameCode(bits: BitArray): GameCode {
+    const entropyBits = bits.slice(-(ENTROPY_BITS + CHECKSUM_BITS), -CHECKSUM_BITS)
+    const entropy = bitsToNumber(entropyBits)
+
+    // Ensure the number of bits matches what the encoding expects
+    while (bits.length % BITS_PER_CHAR !== 0) {
+        bits.unshift(0)
+    }
+
+    const shuffledBits = oneTimePad(bits, entropy)
+    // Keep the original entropy bits intact for decoding
+    shuffledBits.splice(-(ENTROPY_BITS + CHECKSUM_BITS), ENTROPY_BITS, ...entropyBits)
+
+    return encodeBitsToString(shuffledBits)
+}
+
+function gameCodeToBits(code: GameCode): BitArray {
+    const decodedBits = decodeStringToBits(code)
+
+    // get entropy bits and do the one-time pad to get the original bits
+    const entropyBits = decodedBits.slice(-(ENTROPY_BITS + CHECKSUM_BITS), -CHECKSUM_BITS)
+    const entropy = bitsToNumber(entropyBits)
+
+    const unshuffledBits = oneTimePad(decodedBits, entropy)
+    // Replace the entropy bits with the original ones
+    unshuffledBits.splice(-(ENTROPY_BITS + CHECKSUM_BITS), ENTROPY_BITS, ...entropyBits)
+
+    return unshuffledBits
+}
+
+/**
+ * Applies a one-time pad to the input bits using a seed value causing the bits to appear completely random.
+ */
+function oneTimePad(input: BitArray, seed: number): BitArray {
+    const rng = seedrandom(seed.toString())
+    const randBit = () => Math.floor(rng() * 2) as Bit
+    return input.map((bit) => (bit ^ randBit()) as Bit)
 }
 
 function checksum(input: BitArray): number {
--- src/util/number-encoding.ts
+++ src/util/number-encoding.ts
@@ -1,28 +1,42 @@
 import { type BitArray, bitsToString, parseBits } from "@/util/BitArray"
 
 const CHARSET = "xy23456789abcdefghijkzmnopqrstuv"
+const BASE = CHARSET.length
+export const BITS_PER_CHAR = Math.log2(BASE)
+
+if (!Number.isInteger(BITS_PER_CHAR)) throw new Error("Character set length must be a power of 2")
 
 /**
  * Encodes a bit array to a string using distinguishable characters.
  */
 export function encodeBitsToString(bits: BitArray): string {
-    const base = CHARSET.length
-    return bitsToString(bits, base).replace(/./g, (c) => CHARSET[parseInt(c, base)])
+    const numberOfChars = Math.ceil(bits.length / BITS_PER_CHAR)
+    return bitsToString(bits, BASE)
+        .padStart(numberOfChars, "0") // Pad with zeros to ensure correct length
+        .replace(/./g, (c) => CHARSET[parseInt(c, BASE)])
 }
 
 /**
  * Decodes a string back to bits using the same character set as {@link encodeBitsToString}.
  */
 export function decodeStringToBits(encoded: string): BitArray {
-    const base = CHARSET.length
-    const decoded = encoded.toLowerCase().replace(/./g, (c) => {
+    const decoded = encoded.replace(/./g, (c) => {
         let index = CHARSET.indexOf(c)
-        if (index === -1) index = CHARSET.indexOf(commonTypos[c])
+        if (index === -1) index = CHARSET.indexOf(bestGuessIfTypo(c))
         if (index === -1) throw new Error(`Invalid character: ${c}`)
-        return index.toString(base)
+        return index.toString(BASE)
     })
 
-    return parseBits(decoded, base)
+    const numberOfBits = decoded.length * BITS_PER_CHAR
+    const bits = parseBits(decoded, BASE)
+    while (bits.length < numberOfBits) {
+        bits.unshift(0) // Pad with zeros to ensure correct length
+    }
+    return bits
+}
+
+function bestGuessIfTypo(typo: string): string {
+    return commonTypos[typo] || typo.toLowerCase()
 }
 
 const commonTypos: Record<string, string> = {
