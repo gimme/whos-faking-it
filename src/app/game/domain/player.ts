b0121c8c9f33e240373cf9d0f53df29fbb40d8a7
gimmeh <gimmeh@gimmeh.dev>
2025-07-11 14:12:45 +0200

    feat: Allow 2 players
    Impostor distribution:
    - 25%: player 1
    - 25%: player 2
    - 25%: neither
    - 25%: both

--- src/app/game/domain/player.ts
+++ src/app/game/domain/player.ts
@@ -8,12 +8,12 @@ export type Player = {
 /**
  * A number used to identify a seat in a game, one for each player.
  */
-export type SeatNumber = 1 | 2 | SeatCount
+export type SeatNumber = 1 | SeatCount
 
 /**
  * A number of seats used in a game (aka, number of players).
  */
-export type SeatCount = 3 | 4 | 5 | 6 | 7 | 8
+export type SeatCount = 2 | 3 | 4 | 5 | 6 | 7 | 8
 
-export const MIN_SEAT_COUNT: SeatCount = 3
-export const ALL_SEAT_COUNTS: ReadonlyArray<SeatCount> = [3, 4, 5, 6, 7, 8] as const
+export const MIN_SEAT_COUNT: SeatCount = 2
+export const ALL_SEAT_COUNTS: ReadonlyArray<SeatCount> = [2, 3, 4, 5, 6, 7, 8] as const
