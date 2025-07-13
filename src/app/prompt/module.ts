55f3ebeb830519ff9c7f1fed8fcf1afd501d0228
gimmeh <gimmeh@gimmeh.dev>
2025-07-13 17:08:13 +0200

    feat: Allow card limit per module

--- src/app/prompt/module.ts
+++ src/app/prompt/module.ts
@@ -10,14 +10,19 @@ export type Module = {
     readonly promptSpecs?: ReadonlyArray<PromptCardSpec>
     readonly subModules?: ReadonlyArray<Module>
     readonly tags?: NonEmptyArray<Tag>
+    /**
+     * The maximum number of cards that can be generated from this module.
+     * If not specified, there is no limit.
+     */
+    readonly maxCards?: number
 }
 
 export function generateCardsFromModule(module: Module, rng: RNG): ReadonlyArray<Card> {
     const moduleCards = module.promptSpecs?.flatMap((promptSpec) => generateCardsFromSpec(promptSpec, rng)) ?? []
     const subModuleCards = module.subModules?.flatMap((subModule) => generateCardsFromModule(subModule, rng)) ?? []
 
-    const allCards = [...moduleCards, ...subModuleCards]
-    return allCards.map((card) => ({
+    const randomlySelectedCards = [...moduleCards, ...subModuleCards].sort(() => rng() - 0.5).slice(0, module.maxCards)
+    return randomlySelectedCards.map((card) => ({
         ...card,
         tags: createTagSet([...card.tags, ...(module.tags ?? [])]),
     }))
