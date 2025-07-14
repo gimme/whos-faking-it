1f83d90bd01c259b272015e3ee5ec46d1a669b08
gimmeh <gimmeh@gimmeh.dev>
2025-07-14 17:36:11 +0200

    fix: Fix module randomness

--- src/app/prompt/module.ts
+++ src/app/prompt/module.ts
@@ -21,9 +21,27 @@ export function generateCardsFromModule(module: Module, rng: RNG): ReadonlyArray
     const moduleCards = module.promptSpecs?.flatMap((promptSpec) => generateCardsFromSpec(promptSpec, rng)) ?? []
     const subModuleCards = module.subModules?.flatMap((subModule) => generateCardsFromModule(subModule, rng)) ?? []
 
-    const randomlySelectedCards = [...moduleCards, ...subModuleCards].sort(() => rng() - 0.5).slice(0, module.maxCards)
+    const randomlySelectedCards = getRandomSelection([...moduleCards, ...subModuleCards], module.maxCards, rng)
     return randomlySelectedCards.map((card) => ({
         ...card,
         tags: createTagSet([...card.tags, ...(module.tags ?? [])]),
     }))
+}
+
+/**
+ * Returns a random selection of `n` items from the given array.
+ */
+function getRandomSelection<T>(arr: T[], n: number | undefined, rng: RNG): T[] {
+    if (n === undefined || n > arr.length) return [...arr]
+
+    const result = new Array(n)
+    let len = arr.length
+    const taken = new Array(len)
+
+    while (n--) {
+        const r = Math.floor(rng() * len)
+        result[n] = arr[r in taken ? taken[r] : r]
+        taken[r] = --len in taken ? taken[len] : len
+    }
+    return result
 }
